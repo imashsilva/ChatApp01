9e753bcd99e01e9364c3824ab39f97f73360713c
ADMIN <iimashsilva331@gmail.com>
2026-02-16 22:56:00 +0530

    3rd  commit

--- app/src/main/java/lk/silva/chatapp/adapter/ViewPagerAdapter.java
+++ app/src/main/java/lk/silva/chatapp/adapter/ViewPagerAdapter.java
@@ -1,4 +1,31 @@
 package lk.silva.chatapp.adapter;
 
-public class ViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import lk.silva.chatapp.fragment.SignInFragment;
+import lk.silva.chatapp.fragment.SignUpFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager,lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 1) {
+            return new SignInFragment();
+        }
+        return new SignUpFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
 }
